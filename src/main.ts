eb92773bd1c86c2fc6ec4d86e5530448fc2862e5
cydaw6 <56827368+cydaw6@users.noreply.github.com>
2023-02-11 10:42:57 +0100

    1 file modified, 4 added

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,15 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TodoListComponent],
   template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
+    <app-todo-list>
+    </app-todo-list>
   `,
 })
 export class App {
